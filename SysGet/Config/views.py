c805fdb7d6599976e9aa41239c866c1a166a8bd3
Marcos <Markof1089@outlook.com.ar>
2024-10-26 18:46:02 -0300

    Terminando el login de usuario

--- SysGet/Config/views.py
+++ SysGet/Config/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect 
+from django.contrib.auth import authenticate, login 
+from django.http import HttpResponse
 
 def inicio(request):
     lista_usuarios = [
@@ -11,7 +13,7 @@ def inicio(request):
     }
     return render(request, 'index.html', contex)
 
-def login(request):
+def loginUser(request):
 
     #print(request.__dict__) #permite recuperar los parametros en forma de diccionario
     #print(request.GET) # paramtros get 
@@ -21,10 +23,24 @@ def login(request):
     #print (email, password) 
 
     #para metodo POST 
+    username = ""
     if request.method == "POST": 
 
-        email = request.POST.get("email", default = None)
+        username = request.POST.get("username", default = None)
         password = request.POST.get("password", default = None) 
-
-
+        user = authenticate(request, username=username, password=password )
+        print(user)
+        if user is not None:
+            # Iniciar sesión para este usuario
+            login(request, user)
+            return redirect('inicio/')
+        else:
+            # Si las credenciales son incorrectas
+            error_menssage = "Usuario o contraseña incorrectos."
+            contexto = {
+                'error': error_menssage,
+                'username': username, 
+            }
+            return render(request, 'base/login.html', contexto)
+        
     return render(request, 'base/login.html', {})
